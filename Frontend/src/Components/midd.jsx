ffeb4d82ccd094172f5ac5741ceda482c1aba77d
Kendall Norv <piyush.1b.ai@gmail.com>
2026-09-20 18:00:12 +0530

    df

--- Frontend/src/Components/midd.jsx
+++ Frontend/src/Components/midd.jsx
@@ -6,7 +6,7 @@ function Midd() {
         <div className="text-black bg-white">
             <div className="flex flex-col items-center pt-10 md:pt-16 lg:pt-20 px-4 text-center">
                 <p className="font-bold rounded-md text-xs sm:text-sm flex px-4  flex items-center sm:px-3 mt-6 md:mt-10">
-                    🎉New : Use<b ClassName="bg-gray-400 p-2"> PIYUSH0</b> to get 40% Off!
+                    🎉New : Use Top Ai Models in Free!
                 </p>
                 
                 <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold mt-4 md:mt-6">
